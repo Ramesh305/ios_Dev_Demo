d802241e1bbb2ff44b011f3ce35e023b5f8dc00f
= <=>
2021-12-16 20:54:31 +0530

    new name declare

--- DemoGithub/ViewController.swift
+++ DemoGithub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         var count = 100
+        var name = 50
         let myFirstLabel = UILabel()
           let myFirstButton = UIButton()
           myFirstLabel.text = "I made a label on the screen #toogood4you"
